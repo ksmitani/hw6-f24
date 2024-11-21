b743c532e4bca14399acabcd11b1c426d701306f
ksmitani <ksmitani@umich.edu>
2024-11-21 01:42:15 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -8,12 +8,12 @@ window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 	video.autoplay = false; // default to not auto play
 	video.loop = false; // do not loop video
-	videoVolume.innerHTML = `${video.volume * 100}%`;
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play(); // play the video when button is clicked
+	videoVolume.innerHTML = `${video.volume * 100}%`; // display volume
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
